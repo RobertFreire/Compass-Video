c22d823966a5c7eb631482e8f7f2dbb85ee73a17
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-11-03 17:40:10 -0300

    refactor: put props on buttons

--- src/components/MainDetails/ButtonsMain/ButtonsMain.tsx
+++ src/components/MainDetails/ButtonsMain/ButtonsMain.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './ButtonsMain.module.css';
-import MoreInfo from '../../Buttons/MoreInfo/MoreInfo'
+import CustomBtn from '../../Buttons/CustomBtn/CustomBtn'
 import PlusBtn from '../../Buttons/PlusBtn/PlusBtn'
 import FavBtn from '../../Buttons/FavBtn/FavBtn'
 import SeeNow from '../../Buttons/see-now/SeeNow'
@@ -10,7 +10,7 @@ const ButtonsMain = ({ trailer }: { trailer?: boolean }) => {
     return (
         <div className={styles.buttons}>
             <SeeNow />
-            <MoreInfo trailer={trailer} />
+            <CustomBtn trailer={trailer} />
             <div className={styles.circlesButtons}>
             <PlusBtn />
             <FavBtn />
