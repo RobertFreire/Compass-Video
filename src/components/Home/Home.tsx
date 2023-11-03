bbdab4e29be24b7b83bfa39b1ad9967ada31c971
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-11-03 17:24:46 -0300

    refactor: The home page with components and responsive style

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,38 +1,29 @@
 import React from "react";
-import VerAgora from "../Buttons/see-now/VerAgora";
 import styles from "./Home.module.css";
-import MoreInfo from "../Buttons/MoreInfo/MoreInfo";
-import PlusBtn from "../Buttons/PlusBtn/PlusBtn";
-import FavBtn from "../Buttons/FavBtn/FavBtn";
+import Details from "../MainDetails/Details/Details";
+import Overlay from "../MainDetails/Overlay/Overlay";
+import ButtonsMain from "../MainDetails/ButtonsMain/ButtonsMain";
+
 
 const Home = () => {
   return (
     <section className={styles.home}>
-      <div className={styles.content}>
-        <div className={styles.overlay_1}>
-          <div className={styles.overlay_2}>
-            <div className={styles.information}>
-              <div className={styles.details}>
-                <h1 className={styles.title}>Five Nights at Freddy's</h1>
-                <p>2021 • 1 h 41 min</p>
-                <p>Drama, Sci-Fi & Fantasy</p>
-                <p className={styles.description}>
-                  Recently fired and desperate for work, a troubled young man
-                  named Mike agrees to take a position as a night security guard
-                  at an abandoned theme restaurant: Freddy Fazbear's Pizzeria.
-                  But he soon discovers that nothing at Freddy's is what it
-                  seems.
-                </p>
-              </div>
-              <div className={styles.buttons}>
-                <VerAgora/>
-                <MoreInfo/>
-                <PlusBtn/>
-                <FavBtn/>
-              </div>
-            </div>
+      <div className={styles.content} style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg)`}}>
+        <Overlay>
+          <div className={styles.information}>
+           <Details
+            title="Five Nights at Freddy's"
+            year="2021"
+            duration="1 h 41 min"
+            genre="Drama, Sci-Fi & Fantasy"
+            description="Recently fired and desperate for work, a troubled young man
+            named Mike agrees to take a position as a night security guard
+            at an abandoned theme restaurant: Freddy Fazbear's Pizzeria.
+            But he soon discovers that nothing at Freddy's is what it
+            seems." />
+            <ButtonsMain />
           </div>
-        </div>
+        </Overlay>
       </div>
     </section>
   );
