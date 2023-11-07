0facdc09d165d95c1bdc1abced47b7b7392a6bf9
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-11-06 23:44:35 -0300

    refactor: separate the details in components

---
+++ src/Components/MovieInfo/DetailsMedia.tsx
@@ -0,0 +1,42 @@
+import Details from "../MainDetails/Details/Details";
+import { Movie, TVSeries } from "../../shared/information";
+
+function formatYearFromDate(date: string) {
+    const year = new Date(date).getFullYear();
+    return year.toString();
+}
+
+function formatMinutesToHours(minutes: number) {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours}h ${remainingMinutes}min`;
+}
+
+export const MovieDetails = ({ movie }: { movie: Movie }) => {
+    return (
+        <>
+            <Details
+                title={movie.title}
+                year={formatYearFromDate(movie.release_date)}
+                duration={`${formatMinutesToHours(movie.runtime)}`}
+                genre={movie.genres[0].name}
+                description={window.innerWidth >= 768 ? movie.overview : movie.overview.slice(0, 200) + '...'}
+            />
+        </>
+    );
+};
+
+export const TVSeriesDetails = ({ tvSeries }: { tvSeries: TVSeries }) => {
+
+    return (
+        <>
+            <Details
+                title={tvSeries.name}
+                year={formatYearFromDate(tvSeries.first_air_date)}
+                duration={`${tvSeries.number_of_seasons} Temporadas`}
+                genre={tvSeries.genres[0].name}
+                description={window.innerWidth >= 768 ? tvSeries.overview : tvSeries.overview.slice(0, 200) + '...'}
+            />
+        </>
+    );
+};
--- src/Components/MovieInfo/MovieInfo.tsx
+++ src/Components/MovieInfo/MovieInfo.tsx
@@ -1,37 +1,89 @@
-import React from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import styles from "../Home/Home.module.css";
 import Overlay from '../MainDetails/Overlay/Overlay'
 import Details from '../MainDetails/Details/Details'
 import ButtonsMain from '../MainDetails/ButtonsMain/ButtonsMain';
+import { categories, getDetails } from '../../shared/api';
+import { Movie, parseMovie, TVSeries, parseTVSeries} from '../../shared/information';
+import { MovieDetails, TVSeriesDetails } from './DetailsMedia';
 
-const MovieInfo = () => {
 
+const MovieInfo = memo(({ location, MediaId }: { location: string; MediaId?: string }) => {
 
+  const [category, setCategory] = useState<typeof categories>(categories);
+  const [idMovie, setIdMovie] = useState<string>('');
+  const [details, setDetails] = useState<Movie | TVSeries | null>(null);
+  const filterCategory = category.find((item) => item.mediaType === location);
 
+  const Load = async () => {
+    let category = await categories;
+    setCategory(category)
+  }
 
+  const locationPath = () => {
+    if (MediaId) {
+      setIdMovie(() => { return MediaId })
+    }else{
+      const random = Math.floor(Math.random() * filterCategory!.fetchData.results.length);
+      setIdMovie(() => { return filterCategory!.fetchData.results[random].id });
+    }
+  }
+
+  const fetchDetails = async () => {
+    try {
+      if (idMovie) {
+        const details = await getDetails(location, idMovie);
+        if (location === 'movie') {
+          setDetails(parseMovie(details));
+        } else {
+          setDetails(parseTVSeries(details));
+        }
+        
+      }
+    } catch (error) {
+      console.error("Erro ao buscar detalhes:", error);
+    }
+  };
+
+  useEffect(() => {
+    Load();
+  }, []);
+  
+  useEffect(() => {
+    locationPath();
+  }, [MediaId]);
+  
+  useEffect(() => {
+    fetchDetails();
+  }, [idMovie]);
+
+  console.log(details)
+
+  if (details) {
   return (
     <section className={styles.home}>
-    <div className={styles.content} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/t5zCBSB5xMDKcDqe91qahCOUYVV.jpg)` }}>
-      <div className={styles.main}>
-        <Overlay>
-          <div className={styles.information}>
-            <Details
-              title="Five Nights at Freddy's"
-              year="2021"
-              duration="1 h 41 min"
-              genre="Drama, Sci-Fi & Fantasy"
-              description="Recently fired and desperate for work, a troubled young man
-        named Mike agrees to take a position as a night security guard
-        at an abandoned theme restaurant: Freddy Fazbear's Pizzeria.
-        But he soon discovers that nothing at Freddy's is what it
-        seems." />
-            <ButtonsMain />
-          </div>
-        </Overlay>
+      <div className={styles.content} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${details.backdrop_path})`}}>
+        <div className={styles.main}>
+          
+          <Overlay>
+            <div className={styles.information}>
+            {location === 'movie' ? (
+                  <MovieDetails movie={details as Movie} />
+                ) : (
+                  <TVSeriesDetails tvSeries={details as TVSeries} />
+                )}
+              <ButtonsMain trailer={!!MediaId}/>
+            </div>
+          </Overlay>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    
   )
 }
+});
+
+
+
 
 export default MovieInfo
