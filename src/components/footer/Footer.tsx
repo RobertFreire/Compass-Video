2cdc54f77f4641b60a3f34645f7f601c56aef411
FabioGMedeiros <fabio.filho2001@hotmail.com>
2023-11-06 21:06:48 -0300

    fix: footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Footer.css';
+import style from './Footer.module.css';
 import logo from '../../assets/Images/logo_footer.png';
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-            <div className="image_footer">
+        <footer className={style['footer']}>
+            <div className={style['image_footer']}>
                 <img src={logo} alt="UOL_logo" />
             </div>
-            <div className="frame6">
+            <div className={style['frame6']}>
                 <p>Política de privacidad</p>
                 <p>Acuerdo de suscripción</p>
                 <p>Ayuda</p>
@@ -16,7 +16,7 @@ const Footer = () => {
                 <p>Acerca de Disney+</p>
                 <p>Publicidad personalizada</p>
             </div>
-            <div className="frame7">
+            <div className={style['frame7']}>
                 <p>Disney+ es un servicio por suscripción de pago, su contenido está sujeto a disponibilidad. El servicio Disney+ es comercializado por Disney DTC LATAM, Inc., 2400 W Alameda AVE., Burbank CA 91521.</p>
                 <p>© Disney. Todos los derechos reservados.</p>
             </div>
