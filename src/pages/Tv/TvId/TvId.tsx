2f34f23ba3e914a977bd3661c658360a7edb434a
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-11-07 00:16:14 -0300

    feat: Tv Id Page

---
+++ src/pages/Tv/TvId/TvId.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { getDetails, getSimilar } from '../../../shared/api';
+import Header from '../../../components/header/Header';
+import MovieInfo from '../../../components/MovieInfo/MovieInfo';
+import Carrosel from '../../../components/Carrosel/Carrosel';
+import { Season, TVSeries, parseTVSeries } from '../../../shared/information';
+
+const TvId = () => {
+
+    const { id } = useParams();
+    const [details, setDetails] = useState<TVSeries | null>(null);
+    const [similarMovies, setSimilarMovies] = useState();
+
+    const fetchDetails = async () => {
+        try {
+            const details = await getDetails("tv", id!);
+            setDetails(() => parseTVSeries(details));
+        } catch (error) {
+            console.error("Erro ao buscar detalhes:", error);
+        };
+    }
+
+    const fetchSimilarMovies = async () => {
+        try {
+            const data = await getSimilar('tv', id!);
+            setSimilarMovies(() => data);
+        } catch (error) {
+            console.error("Erro ao buscar filmes similares:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchSimilarMovies();
+        fetchDetails();
+    }, [id]);
+
+    console.log(details);
+
+
+    if (details) {
+    return (
+        <div>
+            <Header />
+            <MovieInfo location="tv" MediaId={id} />
+            <Carrosel title='Temporadas' items={details!.seasons} />
+            <Carrosel title='Series Similares' items={similarMovies} location='tv' />
+        </div>
+    )
+}
+}
+export default TvId
