f317d7a1701c5ba80bab1ab41213d6bc1a8040a6
Robert Freire <56230750+RobertFreire@users.noreply.github.com>
2023-11-08 08:53:36 -0300

    feat: added the collection page

---
+++ src/pages/Collections/Colletions.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import Header from '../../components/Header/Header'
+import { useParams } from 'react-router-dom';
+import { Collection, parseCollection, CollectionPart } from '../../shared/information';
+import { getColletions, getDetails } from '../../shared/api';
+import CollectionInfo from '../../components/MovieInfo/CollectionInfo';
+import Footer from '../../components/Footer/Footer';
+import SearchSlider from '../../components/Carrosel/Sliders/SearchSlider';
+
+const Colletions = () => {
+
+    const { id } = useParams();
+    const [details, setDetails] = useState<Collection | null>(null);
+   
+
+    const fetchDetails = async () => {
+        try {
+            const details = await getColletions(id!);
+            setDetails(() => parseCollection(details));
+        } catch (error) {
+            console.error("Erro ao buscar detalhes:", error);
+        };
+    }
+
+    useEffect(() => {
+        fetchDetails();
+    },[]);
+
+    console.log(details);
+
+    return (
+        <>
+            <Header />
+            {details && 
+            <CollectionInfo collection={details}  />}
+            <h1> MAASA</h1>
+            {details &&  
+            <SearchSlider result={details.parts}  />}
+            <Footer />
+        </>
+    )
+}
+
+export default Colletions
